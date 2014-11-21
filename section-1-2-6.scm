4b9045b9e5cfbfcf777825366d91b57e61103660
Alice <alicescarpa@gmail.com>
2014-11-21 18:21:02 -0500

    Partial Section 1.2.6

---
+++ section-1-2-6.scm
@@ -0,0 +1,48 @@
+; Section 1.2.6 - Examples: Testing for Primality
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (square x) (* x x))
+
+(define (divides? a b)
+  (= 0 (remainder b a)))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+; Exercise 1.21
+
+(display (smallest-divisor 199))
+(newline)
+(display (smallest-divisor 1999))
+(newline)
+(display (smallest-divisor 19999))
+(newline)
