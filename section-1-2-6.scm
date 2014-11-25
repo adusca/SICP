d00f21a0877b6711c3863d86e6c2d5d647ae713c
Alice <alicescarpa@gmail.com>
2014-11-25 10:51:08 -0500

    Section 1.2.6 - Testing for Primality - Exercises 1.21 - 127

--- section-1-2-6.scm
+++ section-1-2-6.scm
@@ -69,4 +69,53 @@
 (define (search-for-primes start end)  
   (search-next (if (even? start) (+ start 1) start) end)) 
 
-(search-for-primes 100000 100100)
+(search-for-primes 1000000 1000100)
+
+; Exercise 1.23
+
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
+(define (new-find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (new-find-divisor n (next test-divisor)))))
+
+; Exercise 1.24
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(search-for-primes 1000000 1000100)
+
+#| Exercise 1.25
+
+a = b (mod m) -> a^n = b^n (mod m)
+We used this fact when we defined expmod to avoid dealing with
+numbers larger then m. If we'd used fast-exp we would probably  end up 
+working with larger numbers
+
+Exercise 1.26
+
+The interpreter is going to evaluate each (expmod ...) separately before
+multiplying in all recursive calls, since it is applicative-order
+|#
+
+; Exercise 1.27
+
+(define (carmicheal-test n a)
+    (= (expmod a n n) a))
+
+(define (carmicheal? n)
+  (define (test a)
+    (cond ((= a 1) #t)
+          ((not (carmicheal-test n a)) #f)
+          (else (test (- a 1)))))
+  (test (- n 1)))
+
+(carmicheal? 561)
+
+(carmicheal? 6601)
